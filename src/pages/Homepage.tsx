d45ce65ff22cad1b2cd58c25790def778e277656
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2023-02-23 22:37:36 +0800

    feat: add another homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -10,9 +10,19 @@ interface HomepageProps extends HomepageWrapProps {
   navigate: NavigateFunction;
 }
 
-class Homepage extends React.Component<HomepageProps> {
+interface HomepageState {
+  page: number;
+}
+
+class Homepage extends React.Component<HomepageProps, HomepageState> {
+  interval: ReturnType<typeof setInterval> | undefined;
+
   constructor (props: HomepageProps) {
     super(props);
+
+    this.state = {
+      page: 1
+    };
 
     this.handleScan = this.handleScan.bind(this);
     this.handleError = this.handleError.bind(this);
@@ -30,16 +40,42 @@ class Homepage extends React.Component<HomepageProps> {
     console.error(error);
   }
 
+  componentDidMount () {
+    this.interval = setInterval(() => {
+      const page = this.state.page;
+      this.setState({ page: page === 1 ? 2 : 1 });
+    }, 5000);
+  }
+
   render () {
     return (
       <React.Fragment>
         <img src="/images/logo.png" alt="PRINTU Logo" width={170} id="home-logo" />
         <h1 id="home-welcome">Welcome!</h1>
 
-        <img src="/images/qr.png" alt="PRINTU Website QR Code" width={225} id="home-qr" />
+        {
+          this.state.page === 1 && (
+            <React.Fragment>
+              <img src="/images/qr.png" alt="PRINTU Website QR Code" width={225} id="home-qr" />
 
-        <p className="home-text">JUST ABOUT TO PRINT?</p>
-        <p className="home-text">Scan this code to be redirected to our site or go to <span className="link">www.printu.org</span></p>
+              <p className="home-text">JUST ABOUT TO PRINT?</p>
+              <p className="home-text">Scan this code to be redirected to our site or go to <span className="link">www.printu.org</span></p>
+            </React.Fragment>
+          )
+        }
+
+        {
+          this.state.page === 2 && (
+            <React.Fragment>
+              <div className="home-image-ptr">
+                <img src="/images/welcome.png" alt="PRINTU Welcome" width={225} />
+                <img src="/images/pointer.png" alt="Welcome pointer" width={150} className="home-ptr" />
+              </div>
+
+              <p className="home-text">Scan your QR code in the scanning tray</p>
+            </React.Fragment>
+          )
+        }
 
         <QrReader delay={100} style={{ display: 'none' }} onError={this.handleError} onScan={this.handleScan} />
       </React.Fragment>
