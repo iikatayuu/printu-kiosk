1f432d470624a79227d303ea41402970f2607395
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2023-02-23 14:15:15 +0800

    perf: send initial status

--- server/status.js
+++ server/status.js
@@ -2,11 +2,16 @@
 const cron = require('node-cron')
 const axios = require('axios').default
 
+async function sendStatus () {
+  const data = new FormData()
+  data.set('pass', process.env.ACTIVE_PASS)
+  await axios.post(`${process.env.SERVER_API}/api/active.php`, data)
+}
+
 function start () {
+  sendStatus()
   cron.schedule('*/3 * * * *', async () => {
-    const data = new FormData()
-    data.set('pass', process.env.ACTIVE_PASS)
-    await axios.post(`${process.env.SERVER_API}/api/active.php`, data)
+    await sendStatus()
   })
 }
 
