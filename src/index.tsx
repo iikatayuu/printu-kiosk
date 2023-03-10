03c8a2ec58d5aedf0a1c8891c79e55b8079b4146
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2023-03-10 22:40:53 +0800

    perf: remove strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
@@ -9,9 +8,7 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
